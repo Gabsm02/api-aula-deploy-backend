f5032385cb8847e716b2f38e2d1e6fa92f39820a
Gabriel Mendes <gabrielomendes02@gmail.com>
2023-11-08 11:31:25 -0300

    adicionado banco de dados

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const knex = require("./conexao");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    return res.json("Api está OK!");
+    try {
+        const carros = await knex("carros");
+        return res.json(carros);
+    } catch {
+        return res.status(500).json({ mensagem: "Erro do servidor" });
+    }
 });
 
 app.listen(process.env.PORT);
